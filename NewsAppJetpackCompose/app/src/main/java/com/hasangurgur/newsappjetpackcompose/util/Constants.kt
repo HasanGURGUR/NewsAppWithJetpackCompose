ec791e0d29fd55390c5986d693b28de480cb6b63
HasanGURGUR <hasangurgur95@gmail.com>
2025-05-18 18:56:55 +0300

    -api key hide

--- NewsAppJetpackCompose/app/src/main/java/com/hasangurgur/newsappjetpackcompose/util/Constants.kt
+++ NewsAppJetpackCompose/app/src/main/java/com/hasangurgur/newsappjetpackcompose/util/Constants.kt
@@ -3,6 +3,6 @@ package com.hasangurgur.newsappjetpackcompose.util
 object Constants {
         const val USER_SETTINGS = "userSettings"
         const val APP_ENTRY = "appEntry"
-        const val API_KEY = "ed468ca39bb142adaf4af3fc2a63473a"
+        const val API_KEY = "YOUR_API_KEY"
         const val BASE_URL = "https://newsapi.org/v2/"
 }
